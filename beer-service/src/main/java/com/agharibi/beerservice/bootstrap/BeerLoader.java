cb8bf3d12072851c99c9325fda56bef33e239019
Armen G <gharibi.armen@yahoo.com>
2019-08-16 18:58:37 -0700

    Added beer loader

---
+++ beer-service/src/main/java/com/agharibi/beerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,46 @@
+package com.agharibi.beerservice.bootstrap;
+
+import com.agharibi.beerservice.domain.Beer;
+import com.agharibi.beerservice.repositories.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if (beerRepository.count() == 0) {
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .upc(1234L)
+                    .minOnHand(12)
+                    .price(new BigDecimal("12.99"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .upc(985555L)
+                    .minOnHand(12)
+                    .price(new BigDecimal("10.99"))
+                    .build());
+        }
+    }
+}
