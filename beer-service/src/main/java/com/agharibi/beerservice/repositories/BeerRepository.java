ecf79c8622f169b39cfd19cdae32b3b6e1a878ae
Armen G <gharibi.armen@yahoo.com>
2019-08-16 18:47:48 -0700

    Added BeerJpa repository

---
+++ beer-service/src/main/java/com/agharibi/beerservice/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package com.agharibi.beerservice.repositories;
+
+import com.agharibi.beerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+    
+}
