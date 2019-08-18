aca4fe030d34dbd283d9a1476d3b1b649b89523e
Armen G <gharibi.armen@yahoo.com>
2019-08-18 16:45:38 -0700

    Added customer and customerDto mappers

---
+++ brewery/src/main/java/com/agharibi/brewery/web/mappers/CustomerMapper.java
@@ -0,0 +1,12 @@
+package com.agharibi.brewery.web.mappers;
+
+import com.agharibi.brewery.domain.Customer;
+import com.agharibi.brewery.web.model.CustomerDto;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface CustomerMapper {
+
+    CustomerDto customerToCustomerDto(Customer customer);
+    Customer customerDtoToCustomer(CustomerDto dto);
+}
