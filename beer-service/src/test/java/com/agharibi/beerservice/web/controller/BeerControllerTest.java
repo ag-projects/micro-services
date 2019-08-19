c511dc298e2f5449187cd83a0b5b671c1c5c7b91
Armen G <gharibi.armen@yahoo.com>
2019-08-19 11:17:25 -0700

    more documentations

--- beer-service/src/test/java/com/agharibi/beerservice/web/controller/BeerControllerTest.java
+++ beer-service/src/test/java/com/agharibi/beerservice/web/controller/BeerControllerTest.java
@@ -58,7 +58,7 @@ class BeerControllerTest {
                 .param("isCold", "yes")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(document("v1/beer", pathParameters(
+                .andDo(document("v1/beer-get", pathParameters(
                         parameterWithName("beerId").description("UUID of desired beer to get")),
                     requestParameters(parameterWithName("isCold").description("Is beer cold query param")),
                         responseFields(
@@ -85,7 +85,7 @@ class BeerControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(beerDtoJson))
                         .andExpect(status().isCreated())
-                .andDo(document("v1/beer",
+                .andDo(document("v1/beer-post",
                         requestFields(
                                 fields.withPath("id").ignored(),
                                 fields.withPath("version").ignored(),
