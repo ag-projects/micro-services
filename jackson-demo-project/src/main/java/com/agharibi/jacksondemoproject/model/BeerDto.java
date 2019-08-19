62ed83e986bd31cc92e594f9bb984a6707d2076c
Armen G <gharibi.armen@yahoo.com>
2019-08-19 16:22:39 -0700

    Added Json format for BeerDto

--- jackson-demo-project/src/main/java/com/agharibi/jacksondemoproject/model/BeerDto.java
+++ jackson-demo-project/src/main/java/com/agharibi/jacksondemoproject/model/BeerDto.java
@@ -2,6 +2,9 @@ package com.agharibi.jacksondemoproject.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +14,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -40,4 +44,8 @@ public class BeerDto {
     @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
     private OffsetDateTime lastModifiedDate;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    private LocalDate myLocalDate;
 }
